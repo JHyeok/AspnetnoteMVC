75150761d98b67f258184d3ca415de270a58937f
JHyeok <kjh2522k@gmail.com>
2018-07-22 11:57:35 +0900

    Add MainPage Board

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,14 +5,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AspnetNote.MVC6.Models;
+using AspnetNote.MVC6.DataContext;
+using Microsoft.EntityFrameworkCore;
 
 namespace AspnetNote.MVC6.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            using (var db = new AspnetNoteDbContext())
+            {
+                var MainNote = await db.Notes.Include(uwn => uwn.User).OrderByDescending(uwn => uwn.NoteNo).Take(5).ToListAsync();
+                return View(MainNote);
+            }
         }
 
         public IActionResult LoginSuccess()
